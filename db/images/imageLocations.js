2105c4ac8d74559c50bffe47daad8fdf921cb199
dankk <dankkmemes@gmail.com>
2022-08-19 10:07:03 +0300

    format

---
+++ db/images/imageLocations.js
@@ -0,0 +1,25 @@
+const imagesLocation = "/static/images/"
+
+const galleryImagesLocation = imagesLocation + "gallery/";
+
+const categoriesImagesLocation = imagesLocation + "categories/";
+
+const sliderImagesLocation = imagesLocation + "slider/";
+
+const categoryImagesLocation = {
+
+    koncerti: categoriesImagesLocation + "koncerti/",
+    lazeri: categoriesImagesLocation + "lazeri/",
+    kino: categoriesImagesLocation + "kino/",
+
+}
+
+export {
+
+    imagesLocation,
+    galleryImagesLocation,
+    categoriesImagesLocation,
+    sliderImagesLocation,
+    categoryImagesLocation
+}
+
---
+++ db/images/images.js
@@ -0,0 +1,10 @@
+import { galleryImagesLocation } from "./imageLocations";
+
+const galleryImagesNum = 12;
+
+const galleryImages = Array.from({length: galleryImagesNum}, (_, i) => {
+
+    const curImg = i + 1;
+    return galleryImagesLocation + curImg + ".jpg"
+
+})
